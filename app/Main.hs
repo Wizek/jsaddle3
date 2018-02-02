5b32cd62895d5d52622354f5a20f4ac95b595b39
Milán Nagy <123.wizek@gmail.com>
2018-02-02 06:49:42 +0100

    Reproduced

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,23 @@
 module Main where
 
-import Lib
+import            Data.Text
+import            Control.Monad
+import            Control.Concurrent
+import            Control.Monad.Trans
+import            Control.Monad.IO.Class
+import            Control.Concurrent.MVar
+import            Language.Javascript.JSaddle
+import            Language.Javascript.JSaddle.Warp
+import            Language.Javascript.JSaddle.Debug
 
-main :: IO ()
-main = someFunc
+main = do
+  putStrLn "init"
+  run 3197 $ do
+    result <- liftIO newEmptyMVar
+    deRefVal $ call (eval "(function(f) {f('Hello');})") global [fun $ \ _ _ [arg1] -> do
+      valToText arg1 >>= (liftIO . putMVar result)
+      ]
+    liftIO $ takeMVar result >>= print
+    pure ()
+
+-- TODO https://stackoverflow.com/questions/48571228/is-it-possible-to-break-up-a-long-nix-shell-p-line-into-multiple-lines
